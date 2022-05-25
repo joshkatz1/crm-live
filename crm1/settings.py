88dfc0fa6c50124198ea07a143a35d039a26e4cc
joshkatz1 <joshkatz1@gmail.com>
2022-05-25 18:54:14 +0300

    update

--- crm1/settings.py
+++ crm1/settings.py
@@ -48,11 +48,11 @@ INSTALLED_APPS = [
  
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
